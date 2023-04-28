d66ee341d9e5ae6a58943e02f7f5a341ca334dcb
Feng Xue <xuefeng@snowpeak.org>
2023-04-28 09:46:01 +0800

    Copilot演示文件

--- Copilot/Models.py
+++ Copilot/Models.py
@@ -1,2 +1,12 @@
 from flask_script import Manager
 from app import db
+
+# 创建一个 Speaker 类，包含以下字段： name, bio, linkedin URL
+class Speaker(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    bio = db.Column(db.String(256))
+    linkedin = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<Speaker %r>' % self.name
---
+++ Copilot/snapshot.py
@@ -0,0 +1,6 @@
+# 使用 selenium 配合 chromedriver 作为浏览器，获取网页快照
+# 访问 https://azure.microsoft.com
+# 窗口尺寸为 1920x1080
+# 休眠 3 秒
+# 保存截图为当前年月日时分秒.png
+# 关闭浏览器
