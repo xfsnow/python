b61c1031a8a5da6f97146b774d51b5d5725608bb
Feng Xue <xuefeng@snowpeak.org>
2023-07-17 17:07:18 +0800

    修正 Models.py 文件名大写

--- Copilot/application.py
+++ Copilot/application.py
@@ -1,3 +1,3 @@
-import models
+import Models
 # 实例化一个 Speaker 对象,包含以下字段： name, bio, linkedin URL
-speaker = models.Speaker(name='John Doe', bio='A very nice guy', linkedin='https://www.linkedin.com/in/john-doe-123456789/')
+speaker = Models.Speaker(name='John Doe', bio='A very nice guy', linkedin='https://www.linkedin.com/in/john-doe-123456789/')
