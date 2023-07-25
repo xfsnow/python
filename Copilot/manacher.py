e60e1eacb17c4a29f5bc75a1ae3f84aee9d0e174
Feng Xue <xuefeng@snowpeak.org>
2023-07-25 15:25:32 +0800

    Copilot/manacher.py 把已经写完的内容删掉，以备后面演示 GitHub_Copilot/README.md mitmproxy 用法介绍

--- Copilot/manacher.py
+++ Copilot/manacher.py
@@ -1,44 +1,13 @@
 # Manacher算法，找到输入字符串中最长在回文子字符串
 # 时间复杂度: O(n)
 # 空间复杂度: O(n)
-
+# 输入: 字符串s
+# 返回值: 最长回文子字符串
 def manacher(s):
-    # 预处理字符串
-    s = '#' + '#'.join(s) + '#'
-    # RL数组
-    RL = [0] * len(s)
-    # 当前最右回文串的中心位置，及其右边界
-    pos, max_right = 0, 0
-    # 最长回文串的中心位置，及其长度
-    max_pos, max_len = 0, 0
-    for i in range(len(s)):
-        # 初始化RL[i]
-        if i < max_right:
-            RL[i] = min(RL[2 * pos - i], max_right - i)
-        else:
-            RL[i] = 1
-        # 尝试扩展RL[i]
-        while i - RL[i] >= 0 and i + RL[i] < len(s) and s[i - RL[i]] == s[i + RL[i]]:
-            RL[i] += 1
-        # 更新pos和max_right
-        if RL[i] + i - 1 > max_right:
-            max_right = RL[i] + i - 1
-            pos = i
-        # 更新max_pos和max_len
-        if RL[i] > max_len:
-            max_len = RL[i]
-            max_pos = i
-    # 返回最长回文子串
-    return s[max_pos - max_len + 1:max_pos + max_len:2]
 
-   
+       
 # 单元测试
-def manacher_test():
-    print(manacher('abba'))
-    print(manacher('abababa'))
-    print(manacher('abcbabcbabcba'))
-    print(manacher('测试中文文中有多少字符？'))
+# def manacher_test():
 
 
-if __name__ == '__main__':
-    manacher_test()
+
---
+++ GitHub_Copilot/README.md
@@ -0,0 +1,13 @@
+# GitHub Copilot 相关功能
+
+## 使用 mitmproxy 配置代理及跟踪传输内容
+1. 在 Ubuntu 安装
+pip install mitmproxy
+2. 上传 proxy_confyg.py 文件到云虚机
+3. 启动 mitmproxy
+`nohup mitmdump --set block_global=false -s proxy_config.py &`
+由于 mitmproxy 默认运行在 8080 端口，如果是运行在云虚机上，需要开启 8080 端口的网络安全组。
+
+4. 在 IDE 中配置代理服务器
+Http Proxy: 填写 mitmproxy 对外的IP，形如 http://12.34.56.78:8080
+Http Proxy Strict SSL 不勾选
