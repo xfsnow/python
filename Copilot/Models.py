c88631787ff4536722894964baa97747ce41e252
Feng Xue <xuefeng@snowpeak.org>
2023-09-07 13:23:21 +0800

    演示Copilot的脚本

--- Copilot/Models.py
+++ Copilot/Models.py
@@ -3,8 +3,3 @@ from app import db
 
 # 创建一个 Speaker 类，包含以下字段： name, bio, linkedin URL
 class Speaker(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
-    bio = db.Column(db.String(256))
-    linkedin = db.Column(db.String(256))
-    
--- Copilot/application.py
+++ Copilot/application.py
@@ -1,3 +1,2 @@
 import Models
 # 实例化一个 Speaker 对象,包含以下字段： name, bio, linkedin URL
-speaker = Models.Speaker(name='John Doe', bio='A very nice guy', linkedin='https://www.linkedin.com/in/john-doe-123456789/')
